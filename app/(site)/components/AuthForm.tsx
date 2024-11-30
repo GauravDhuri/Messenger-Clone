1f76c571ee3b5c1fba75406e3c299a198a80e14d
Gaurav Dhuri <dhuriofficial@gmail.com>
2024-12-01 02:34:04 +0530

    Added prisma and mongo setup for database and UI changes for social auth login

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -4,6 +4,9 @@ import Button from '@/app/components/Button';
 import Input from '@/app/components/input/Input';
 import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import AuthSocialButton from './AuthSocialButton';
+import { BsGithub, BsGoogle } from 'react-icons/bs';
+import axios from 'axios';
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
@@ -34,7 +37,7 @@ const AuthForm = () => {
     setIsLoading(true);
 
     if (varaint === 'REGISTER') {
-      // Axios call
+      axios.post('/api/register', data)
     }
 
     if (varaint === 'LOGIN') {
@@ -53,15 +56,16 @@ const AuthForm = () => {
       <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
           {varaint === 'REGISTER' && (
-            <Input id="name" label="Name" register={register} errors={errors} />
+            <Input id="name" label="Name" register={register} errors={errors} disabled={isLoading} />
           )}
-          <Input id="email" label="Email" type="email" register={register} errors={errors} />
+          <Input id="email" label="Email" type="email" register={register} errors={errors} disabled={isLoading} />
           <Input
             id="password"
             label="Password"
             type="password"
             register={register}
             errors={errors}
+            disabled={isLoading}
           />
           <div>
             <Button disabled={isLoading} fullWidth type="submit">
@@ -71,13 +75,30 @@ const AuthForm = () => {
         </form>
         <div className="mt-6">
           <div className="relative">
-            <div className="item-center absolute inset-0 flex">
-              <div className="w-full border-t border-gray-300">
-                <div className="relative flex justify-center text-sm">
-                  <span className="bg-white px-2 text-gray-500">Or continue with</span>
-                </div>
-              </div>
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
             </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">Or continue with</span>
+            </div>
+          </div>
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton 
+              icon={BsGithub}
+              onClick={() => socialAction('github')}
+            />
+            <AuthSocialButton 
+              icon={BsGoogle}
+              onClick={() => socialAction('google')}
+            />
+          </div>
+        </div>
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          <div>
+            {varaint === 'LOGIN' ? 'New to Messenger ?' : 'Already have an account ?'}
+          </div>
+          <div onClick={toggleVaraint} className="underline cursor-pointer">
+            {varaint === 'LOGIN' ? 'Create an account' : 'Login'}
           </div>
         </div>
       </div>
